be321deddc228eb305105b81663ccff6ed15d35b
김종원 <dobuzi@gimjong-won-ui-MacBookPro.local>
2020-05-05 20:56:30 +0900

    Small change

--- clock.js
+++ clock.js
@@ -3,11 +3,14 @@ const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
     const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds}`;
+    const hours = setClockUnits(date.getHours());
+    const minutes = setClockUnits(date.getMinutes());
+    const seconds = setClockUnits(date.getSeconds());
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function setClockUnits(time) {
+    return time < 10 ? `0${time}` : time
 }
 
 function init() {
